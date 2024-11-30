2f2d6866e275017a3092c7557a77dc76ca815c1c
it27427 <istiak.tushar.qtec@gmail.com>
2024-12-01 00:14:31 +0600

    Logo Size Updated

--- src/components/client/logo/Logo.jsx
+++ src/components/client/logo/Logo.jsx
@@ -5,7 +5,7 @@ import styles from './logo.module.css';
 const Logo = () => {
   return (
     <figure className={styles.brandlogo}>
-      <Image src={BrandLogo} alt='Brand Logo' width={96} height={60} className={styles.logo} />
+      <Image src={BrandLogo} alt='Brand Logo' width={96} height={60} priority className={styles.logo} />
     </figure>
   )
 }
