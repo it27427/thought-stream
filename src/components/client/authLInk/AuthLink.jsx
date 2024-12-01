500789cde47fc2f9ed1d848f0ee9f22be16c8f02
it27427 <istiak.tushar.qtec@gmail.com>
2024-12-01 16:06:03 +0600

    AuthLinks Conditions Added

--- src/components/client/authLInk/AuthLink.jsx
+++ src/components/client/authLInk/AuthLink.jsx
@@ -2,11 +2,27 @@ import styles from './authlink.module.css';
 import Link from 'next/link';
 
 const AuthLink = () => {
+  const status = 'notauthenticated';
+
   return (
     <>
-      <Link href='#' className={styles.loginbutton}>
-        <span>Login</span>
-      </Link>
+      {
+        status === 'notauthenticated' ? (
+          <Link href='/login' className={styles.loginbutton}>
+            <span>Login</span>
+          </Link>
+        ) : (
+          <>
+            <Link href='/write' className={styles.loginbutton}>
+              <span>Write</span>
+            </Link>
+
+            <button className={styles.logoutbutton}>
+              <span>LogOut</span>
+            </button>
+          </>
+        )
+      }
     </>
   )
 }
